a63e18ee32aaabb2ff8daf9d6a89421387e4cf6b
andreidrobota <drobotaandrei@gmail.com>
2024-02-01 08:18:07 +0000

    Add test post from admin panel

--- newspage/models.py
+++ newspage/models.py
@@ -14,3 +14,4 @@ class News(models.Model):
     content = models.TextField()
     posting_date = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    teaser = models.TextField(blank=True)
