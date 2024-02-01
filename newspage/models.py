12aa99e471c6156ec7b55a3a342ffc85556fa3dc
andreidrobota <drobotaandrei@gmail.com>
2024-02-01 14:06:12 +0000

    Comment model created

--- newspage/models.py
+++ newspage/models.py
@@ -15,3 +15,16 @@ class News(models.Model):
     posting_date = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     teaser = models.TextField(blank=True)
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        News, on_delete=models.CASCADE, related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    added_on = models.DateTimeField(auto_now_add=True)
