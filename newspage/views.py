07ac799c66f8053b8736915ed2bf251589a4f9ec
andreidrobota <drobotaandrei@gmail.com>
2024-02-02 08:32:30 +0000

    news_detail view

--- newspage/views.py
+++ newspage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import News
 
@@ -8,3 +8,9 @@ class NewsList(generic.ListView):
     queryset = News.objects.filter(status=1)
     template_name = "newspage/index.html"
     paginate_by = 3
+
+
+def news_detail(request,slug):
+    queryset = News.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "newspage/news_detail.html", {"post": post},)
